3da59313282b48632a448dd4735b8efcc73abe27
Trần Văn Trường <ngo2016@gmail.com>
2020-10-16 06:20:26 +0700

    set the enemy humanoid agent type to make it move on mavmesh area, add second stage to main menu

--- Assets/AIBehavior/ExampleScripts/PlayerStats.cs
+++ Assets/AIBehavior/ExampleScripts/PlayerStats.cs
@@ -3,59 +3,59 @@ using UnityEngine.UI;
 
 namespace AIBehaviorExamples
 {
-	public class PlayerStats : MonoBehaviour
-	{
+    public class PlayerStats : MonoBehaviour
+    {
         [SerializeField] private AudioSource _audioSource;
         [SerializeField] private AudioSource loopAudio;
-		public float health = 100.0f;
-		[SerializeField] AudioClip hurtSound;
-		[SerializeField] AudioClip deadSound;
-		[SerializeField] GameObject hurtImg;
-		[SerializeField] Text scoreText;
-		[SerializeField] int _score;
+        public float health = 100.0f;
+        [SerializeField] AudioClip hurtSound;
+        [SerializeField] AudioClip deadSound;
+        [SerializeField] GameObject hurtImg;
+        [SerializeField] Text scoreText;
+        [SerializeField] int _score;
 
-		public void SubtractHealth(float amount)
-		{
-			health -= amount;
+        public void SubtractHealth(float amount)
+        {
+            health -= amount;
 
-			if ( health <= 0.0f )
-			{
-				health = 0.0f;
-				Debug.LogWarning("You're Dead!");
-				//Disable loop audio
-				loopAudio.enabled = false;
-				//Play dead sound
-				_audioSource.PlayOneShot(deadSound);
-				//Remove tag
-				gameObject.tag = "Untagged";
-				//Find Game Manager and put an end to this
-				FindObjectOfType<GameManager>().GameOver();
-			}
-			else
-			{
-				Debug.Log("Health is now: " + health);
-				//Play hurt sound
-				_audioSource.PlayOneShot(hurtSound);
-                if (health<=50)
+            if (health <= 0.0f)
+            {
+                health = 0.0f;
+                Debug.LogWarning("You're Dead!");
+                //Disable loop audio
+                loopAudio.enabled = false;
+                //Play dead sound
+                _audioSource.PlayOneShot(deadSound);
+                //Remove tag
+                gameObject.tag = "Untagged";
+                //Find Game Manager and put an end to this
+                FindObjectOfType<GameManager>().GameOver();
+            }
+            else
+            {
+                Debug.Log("Health is now: " + health);
+                //Play hurt sound
+                _audioSource.PlayOneShot(hurtSound);
+                if (health <= 50)
                 {
-					loopAudio.Play();
-					var color = hurtImg.GetComponent<Image>().color;
-					color.a = 0.5f;
-					hurtImg.GetComponent<Image>().color = color;
+                    loopAudio.Play();
+                    var color = hurtImg.GetComponent<Image>().color;
+                    color.a = 0.5f;
+                    hurtImg.GetComponent<Image>().color = color;
                 }
-			}
-		}
+            }
+        }
 
         public void Damage(float damage)
-		{
-			Debug.Log("Got hit with " + damage + " damage points");
-			SubtractHealth(damage);
-		}
-
-		void Score(int score)
         {
-			_score += score;
-			scoreText.text = _score.ToString();
+            Debug.Log("Got hit with " + damage + " damage points");
+            SubtractHealth(damage);
         }
-	}
+
+        void Score(int score)
+        {
+            _score += score;
+            scoreText.text = _score.ToString();
+        }
+    }
 }
