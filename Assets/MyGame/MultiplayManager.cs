ae50e812bf6a37fae74e9406db6cc4bb752be400
Trần Văn Trường <ngo2016@gmail.com>
2020-11-29 16:34:48 +0700

    implementing multiplayer, fix null ref at stage 2 and 3

---
+++ Assets/MyGame/MultiplayManager.cs
@@ -0,0 +1,31 @@
+﻿using Firebase.Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MultiplayManager : MonoBehaviour
+{
+    [SerializeField]
+    private string mainMenuScene;
+
+    DatabaseReference reference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackToSingle()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
