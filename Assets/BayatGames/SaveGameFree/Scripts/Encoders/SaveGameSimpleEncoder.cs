e13a3e66889f4ad1b89a66a497624e180e724c8c
Trần Văn Trường <ngo2016@gmail.com>
2020-12-02 22:23:39 +0700

    add share to social network

--- Assets/BayatGames/SaveGameFree/Scripts/Encoders/SaveGameSimpleEncoder.cs
+++ Assets/BayatGames/SaveGameFree/Scripts/Encoders/SaveGameSimpleEncoder.cs
@@ -13,8 +13,9 @@ namespace BayatGames.SaveGameFree.Encoders
 	/// </summary>
 	public class SaveGameSimpleEncoder : ISaveGameEncoder
 	{
-	
+		// This constant is used to determine the keysize of the encryption algorithm in bits.
 		private const int Keysize = 256;
+		// This constant determines the number of iterations for the password bytes generation function
 		private const int DerivationIterations = 1000;
 
 		/// <summary>
@@ -25,6 +26,8 @@ namespace BayatGames.SaveGameFree.Encoders
 		public string Encode ( string input, string password )
 		{
 			#if !UNITY_WSA || !UNITY_WINRT
+			// Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.
 			var saltStringBytes = Generate256BitsOfRandomEntropy ();
 			var ivStringBytes = Generate256BitsOfRandomEntropy ();
 			var plainTextBytes = Encoding.UTF8.GetBytes ( input );
@@ -43,6 +46,7 @@ namespace BayatGames.SaveGameFree.Encoders
 						{
 							cryptoStream.Write ( plainTextBytes, 0, plainTextBytes.Length );
 							cryptoStream.FlushFinalBlock ();
+							// Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
 							var cipherTextBytes = saltStringBytes;
 							cipherTextBytes = cipherTextBytes.Concat ( ivStringBytes ).ToArray ();
 							cipherTextBytes = cipherTextBytes.Concat ( memoryStream.ToArray () ).ToArray ();
@@ -66,9 +70,14 @@ namespace BayatGames.SaveGameFree.Encoders
 		public string Decode ( string input, string password )
 		{
 			#if !UNITY_WSA || !UNITY_WINRT
-			var cipherTextBytesWithSaltAndIv = Convert.FromBase64String ( input );
+			// Get the complete stream of bytes that represent:
+            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String ( input );
+			// Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
 			var saltStringBytes = cipherTextBytesWithSaltAndIv.Take ( Keysize / 8 ).ToArray ();
+			// Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
 			var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip ( Keysize / 8 ).Take ( Keysize / 8 ).ToArray ();
+			// Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
 			var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip ( ( Keysize / 8 ) * 2 ).Take ( cipherTextBytesWithSaltAndIv.Length - ( ( Keysize / 8 ) * 2 ) ).ToArray ();
 			var passPhrase = new Rfc2898DeriveBytes ( password, saltStringBytes, DerivationIterations );
 			var keyBytes = passPhrase.GetBytes ( Keysize / 8 );
@@ -100,8 +109,9 @@ namespace BayatGames.SaveGameFree.Encoders
 		#if !UNITY_WSA || !UNITY_WINRT
 		private static byte[] Generate256BitsOfRandomEntropy ()
 		{
-			var randomBytes = new byte[32];
+			var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
 			var rngCsp = new RNGCryptoServiceProvider ();
+			// Fill the array with cryptographically secure random bytes.
 			rngCsp.GetBytes ( randomBytes );
 			return randomBytes;
 		}
---
+++ Assets/MyGame/ShareScoreManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelBusters;
+using VoxelBusters.NativePlugins;
+
+public class ShareScoreManager : MonoBehaviour
+{
+    private bool isSharing = false;
+
+    public void ShareSocialMedia()
+    {
+        isSharing = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (isSharing)
+        {
+            isSharing = false;
+            StartCoroutine(CaptureScreenShoot());
+        }
+    }
+
+    private IEnumerator CaptureScreenShoot()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+        ShareSheet(texture);
+        Destroy(texture);
+    }
+
+    public void ShareSheet(Texture2D texture)
+    {
+        ShareSheet shareSheet = new ShareSheet();
+        shareSheet.Text = "Share Sheet";
+        shareSheet.AttachImage(texture);
+        shareSheet.URL = "https://twiiter.com/RoixoGames";
+
+        NPBinding.Sharing.ShowView(shareSheet, FinishSharing);
+    }
+
+    private void FinishSharing(eShareResult _result)
+    {
+        Debug.Log(_result);
+    }
+}
